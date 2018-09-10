8fb1330c081cf4868d3f96d1aa2754e7e85ec485
Jude Niroshan <jude.niroshan@cambio.lk>
2018-09-10 12:02:25 +0530

    changed POST to GET in CSRFProvider

--- src/org/csrf/sync/attack/servlets/CSRFProvider.java
+++ src/org/csrf/sync/attack/servlets/CSRFProvider.java
@@ -27,11 +27,6 @@ public class CSRFProvider extends HttpServlet
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
-    response.getWriter().append("Served at: ").append(request.getContextPath());
-  }
-
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-  {
     HttpSession session = request.getSession(false);
 
     if (session != null)
